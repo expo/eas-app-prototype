beaf2429cda7cb14c723acaf43969a8211b4960d
gabrieldonadel <donadeldev@gmail.com>
2023-01-05 17:51:56 -0300

    Add useDownloadBuild hook

---
+++ hooks/useDownloadBuild.ts
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { Platform } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { startActivityAsync } from "expo-intent-launcher";
+
+export enum DownloadStatus {
+  COMPLETED = "completed",
+  ERROR = "error",
+  IN_PROGRESS = "in_progress",
+  PAUSED = "paused",
+}
+
+interface UseDownloadBuildParams {
+  buildUrl: string;
+}
+
+export const useDownloadBuild = ({ buildUrl }: UseDownloadBuildParams) => {
+  const [localUri, setLocalUri] = useState<string>(undefined);
+  const [progress, setProgress] = useState<number>();
+  const [status, setStatus] = useState<DownloadStatus>();
+  const buildName = buildUrl?.substring(buildUrl?.lastIndexOf("/") + 1);
+
+  const downloadResumable = FileSystem.createDownloadResumable(
+    buildUrl,
+    FileSystem.documentDirectory + buildName,
+    {},
+    (downloadProgress) => {
+      const progress =
+        downloadProgress.totalBytesWritten /
+        downloadProgress.totalBytesExpectedToWrite;
+      setProgress(progress);
+    }
+  );
+
+  const downloadBuild = async () => {
+    let buildUri = localUri;
+
+    if (!buildUri) {
+      setStatus(DownloadStatus.IN_PROGRESS);
+      try {
+        const fileSystemDownloadResult = progress
+          ? await downloadResumable.downloadAsync()
+          : await downloadResumable.resumeAsync();
+
+        if (!fileSystemDownloadResult) {
+          return;
+        }
+
+        buildUri = fileSystemDownloadResult.uri;
+        setLocalUri(buildUri);
+
+        setStatus(DownloadStatus.COMPLETED);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    return installBuild(localUri);
+  };
+
+  const pauseDownload = async () => {
+    try {
+      setStatus(DownloadStatus.PAUSED);
+      await downloadResumable.pauseAsync();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return {
+    downloadBuild,
+    pauseDownload,
+    progress,
+    status,
+  };
+};
+
+const installBuild = async (uri: string) => {
+  if (Platform.OS === "android") {
+    const cUri = await FileSystem.getContentUriAsync(uri);
+
+    await startActivityAsync("android.intent.action.VIEW", {
+      data: cUri,
+      type: "application/vnd.android.package-archive",
+      flags: 1,
+    });
+    return;
+  }
+
+  if (Platform.OS === "ios") {
+  }
+};
